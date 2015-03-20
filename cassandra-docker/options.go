78cf4495e12d40bd64d3b5118cdc6fd2d9c50208
Al Tobey <tobert@gmail.com>
2015-03-20 16:47:06 -0700

    update/add license/copyright

--- cassandra-docker/options.go
+++ cassandra-docker/options.go
@@ -1,11 +1,25 @@
 package main
 
-// pull entrypoint-specific options out of the os.Args
-// so the rest can be passed through to backing tools
+/*
+ * Copyright 2015 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Pull entrypoint-specific options out of the os.Args
+ * so the rest can be passed through to backing tools.
+ */
 
-import (
-	"strings"
-)
+import "strings"
 
 func deleteArg(args []string, index int) ([]string, string) {
 	var deleted string
